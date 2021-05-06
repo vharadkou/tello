dfff388cb87d513103d951249a826978377a34ed
Valiantsin <valiantsin.haradkou@icloud.com>
2021-05-06 23:13:15 +0300

    feat: deployment to aws

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,13 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
-@Controller()
+import sdk from '@0x77/tellots';
+
+@Controller('commands')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Post('connect')
+  async connect() {
+    try {
+      await sdk.control.connect();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  @Post('takeoff')
+  async takeOff() {
+    try {
+      await sdk.control.takeOff();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  @Post('land')
+  async land() {
+    try {
+      await sdk.control.land();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  @Post('flip')
+  async flip() {
+    try {
+      await sdk.control.flip.front();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  @Get('battery')
+  async battery() {
+    const battery = await sdk.read.battery();
+
+    return battery;
   }
 }
